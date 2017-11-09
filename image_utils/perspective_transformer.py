ab7c78746dc829bf627cc753a8c2063e42b14905
William Gillespie <wgilles1@emich.edu>
2017-11-09 18:07:23 -0500

    Corrected spelling in method of perspective transformer.
    Also deleted some old code.

--- image_utils/perspective_transformer.py
+++ image_utils/perspective_transformer.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 
-def make_perpective_transform(gray_image, src_pts, dst_pts):
+def make_perspective_transform(gray_image, src_pts, dst_pts):
     src_pts = np.float32(src_pts[0])
     dst_pts = np.float32(dst_pts[0])
     M = cv2.getPerspectiveTransform(src=src_pts, dst=dst_pts)
@@ -11,6 +11,7 @@ def make_perpective_transform(gray_image, src_pts, dst_pts):
     img_size = (gray_image.shape[1], gray_image.shape[0])
     warped_image = cv2.warpPerspective(gray_image, M, img_size)
     return warped_image, M, Minv
+
 
 def make_rgb_perspective_transform(rgb_img, src_pts, dst_pts):
     src_pts = np.float32(src_pts[0])
@@ -36,20 +37,6 @@ def source_points(gray_image):
             ]
         ], dtype=np.int32)
     return src
-
-# def source_points(gray_image):
-#     x_size = gray_image.shape[1]
-#     y_size = gray_image.shape[0]
-#     src = np.array(
-#         [
-#             [
-#                 [(x_size / 2) - 55, y_size / 2 + 100],
-#                 [((x_size / 6) - 10), y_size],
-#                 [(x_size * 5 / 6) + 60, y_size],
-#                 [(x_size / 2 + 55), y_size / 2 + 100]
-#             ]
-#         ], dtype=np.int32)
-#     return src
 
 
 def destination_points(gray_image):
@@ -88,43 +75,3 @@ def region_mask_points(gray_image):
         dtype=np.int32
     )
     return src_points
-
-# def destination_points(gray_image):
-#     x_size = gray_image.shape[1]
-#     y_size = gray_image.shape[0]
-#     bottom_left = (x_size * 0.1, y_size)
-#     top_left = (x_size * .1, 0.0)
-#     top_right = (x_size * .9, y_size)
-#     bottom_right = (x_size * 0.9, 0.0)
-#     dest_points = np.array(
-#         [
-#             [
-#                 bottom_left,
-#                 top_left,
-#                 bottom_right,
-#                 top_right
-#             ]
-#         ],
-#         dtype=np.int32
-#     )
-#     return dest_points
-
-# def destination_points(gray_image):
-#     x_size = gray_image.shape[1]
-#     y_size = gray_image.shape[0]
-#     bottom_left = (x_size * 0.25, y_size)
-#     top_left = (x_size * .25, 0.0)
-#     top_right = (x_size * .75, y_size)
-#     bottom_right = (x_size * 0.75, 0.0)
-#     dest_points = np.array(
-#         [
-#             [
-#                 bottom_left,
-#                 top_left,
-#                 bottom_right,
-#                 top_right
-#             ]
-#         ],
-#         dtype=np.int32
-#     )
-#     return dest_points
